33b2bb4d21c59ed6947aa59d1e0bbdfb5859d4c5
Isaac Curtis <iicurtis@outlook.com>
2024-06-05 17:40:35 -0600

    ploopyco/trackball: momentary dragscroll

--- keyboards/ploopyco/trackball/keymaps/iicurtis/keymap.c
+++ keyboards/ploopyco/trackball/keymaps/iicurtis/keymap.c
@@ -20,5 +20,5 @@
 // safe range starts at `PLOOPY_SAFE_RANGE` instead.
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT(/* Base */
-                                                                           KC_BTN1, KC_BTN3, KC_BTN2, KC_BTN4, LT(1, KC_BTN5)),
+                                                                           KC_BTN1, KC_BTN3, KC_BTN2, LT(1, KC_BTN4), LT(1, KC_BTN5)),
                                                               [1] = LAYOUT(DRAG_SCROLL, _______, DPI_CONFIG, DRAG_SCROLL, DRAG_SCROLL)};
---
+++ keyboards/ploopyco/trackball/keymaps/iicurtis/rules.mk
@@ -0,0 +1,9 @@
+# Processor frequency
+F_CPU = 8000000
+
+POINTING_DEVICE_DRIVER = pmw3360
+
+DEFAULT_FOLDER = ploopyco/trackball/rev1_005
+
+# TAP_DANCE_ENABLE = yes
+# MOUSEKEY_ENABLE = yes
